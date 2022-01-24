984f4f0aa34390f53dcf796063665090f9c642b9
vivek.pal <vivek.pal@gds.ey.com>
2022-01-24 19:51:35 +0530

    code updated

--- Animations/app/src/main/java/com/example/animations/MainActivity.java
+++ Animations/app/src/main/java/com/example/animations/MainActivity.java
@@ -3,8 +3,31 @@ package com.example.animations;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isHomerShowing = true;
+    public void fade(View view){
+
+        ImageView homerImageView = (ImageView)findViewById(R.id.homerImageView);
+        ImageView bartImageView = (ImageView)findViewById(R.id.bartImageView);
+
+
+        if (isHomerShowing){
+            isHomerShowing = false;
+            homerImageView.animate().alpha(0).setDuration(2000);
+            bartImageView.animate().alpha(1).setDuration(2000);
+        }
+        else{
+            isHomerShowing = true;
+            homerImageView.animate().alpha(1).setDuration(2000);
+            bartImageView.animate().alpha(0).setDuration(2000);
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
